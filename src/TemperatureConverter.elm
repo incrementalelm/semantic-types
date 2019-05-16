35459f42ceea87c18b3afca6399d03d78f13e9fb
Dillon Kearns <dillon@dillonkearns.com>
2019-05-16 15:30:19 -0700

    Update temperature example boilerplate.

--- src/TemperatureConverter.elm
+++ src/TemperatureConverter.elm
@@ -8,7 +8,6 @@ import Element.Input
 import Html exposing (Html, button, div, h1, p, text)
 import Html.Events exposing (onClick)
 import Json.Decode
-import RawSearch exposing (RawSearch)
 
 
 type alias Flags =
@@ -16,17 +15,17 @@ type alias Flags =
 
 
 type alias Model =
-    { inputValue : RawSearch }
+    { temperature : Maybe Float }
 
 
 type Msg
     = NoOp
-    | OnInput RawSearch
+    | OnInput String
 
 
 init : () -> ( Model, Cmd msg )
 init () =
-    ( { inputValue = RawSearch.empty }, Cmd.none )
+    ( { temperature = Nothing }, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
@@ -40,41 +39,11 @@ mainView : Model -> Element Msg
 mainView model =
     Element.column
         [ Element.spacing 30, Element.centerX, Element.width Element.fill ]
-        [ toEncode model.inputValue
-        , toDecode model.inputValue
-        , Element.newTabLink []
-            { url = googleLink model.inputValue
-            , label =
-                Element.text
-                    ("Search Google for \""
-                        ++ RawSearch.unencoded model.inputValue
-                        ++ "\""
-                    )
-            }
+        [ model.temperature
+            |> Maybe.withDefault 0
+            |> String.fromFloat
+            |> Element.text
         ]
-
-
-googleLink : RawSearch -> String
-googleLink inputValue =
-    "https://google.com/search?q=" ++ RawSearch.encoded inputValue
-
-
-toEncode : RawSearch -> Element Msg
-toEncode inputValue =
-    RawSearch.input [ Element.width Element.fill ]
-        (Element.Input.labelAbove [] (Element.text "Raw text (Not Encoded)"))
-        inputValue
-        OnInput
-
-
-toDecode : RawSearch -> Element Msg
-toDecode rawValue =
-    Element.Input.text [ Element.width Element.fill ]
-        { onChange = \_ -> NoOp
-        , text = RawSearch.encoded rawValue
-        , placeholder = Nothing
-        , label = Element.Input.labelAbove [] (Element.text "Valid URL (Encoded)")
-        }
 
 
 main : Program Flags Model Msg
@@ -94,4 +63,4 @@ update msg model =
             ( model, Cmd.none )
 
         OnInput newInput ->
-            ( { model | inputValue = newInput }, Cmd.none )
+            ( { model | temperature = Nothing }, Cmd.none )
