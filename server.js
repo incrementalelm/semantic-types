daaed12a705fea4e75f56997502028aadf2423ad
Dillon Kearns <dillon@dillonkearns.com>
2019-05-30 09:38:03 -0700

    Respond to server requests with no query params with the saved SSN.

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const http = require("http"),
   server = http.createServer();
 
 server.on("request", (request, response) => {
-  const ssn = url.parse(request.url, true).query.ssn;
-  const ssnWithoutDashes = ssn.replace(new RegExp(/-/, "g"), "");
+  const parsedUrl = url.parse(request.url, true);
+  if (parsedUrl.query && parsedUrl.query.ssn) {
+    const ssn = parsedUrl.query.ssn;
+    const ssnWithoutDashes = ssn.replace(new RegExp(/-/, "g"), "");
 
-  response.writeHead(200, { "Content-Type": "application/json" });
-  if (/^([0-9\-]*)$/.test(ssnWithoutDashes)) {
-    response.end(JSON.stringify({ error: null }));
+    response.writeHead(200, { "Content-Type": "application/json" });
+    if (/^([0-9\-]*)$/.test(ssnWithoutDashes)) {
+      response.end(JSON.stringify({ error: null }));
+      global.ssn = ssn;
+    } else {
+      response.end(
+        JSON.stringify({ error: `Contains characters besides - and [0-9].` })
+      );
+    }
   } else {
-    response.end(
-      JSON.stringify({ error: `Contains characters besides - and [0-9].` })
-    );
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ ssnInput: global.ssn }));
   }
 });
 
